c63c95f22e61cd7adf29f66d00d7942716bbe8ba
uzairname <uzair.m6d@gmail.com>
2025-09-14 13:18:22 -0400

    rm note_modules .next .turbo

--- apps/dashboard/.next/static/chunks/apps_dashboard_src_app_favicon_ico_mjs_9dcc8202._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/4787e_next_dist_c67751b4._.js"
-],
-    source: "dynamic"
-});
